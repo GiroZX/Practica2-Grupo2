8289ca8ab0010ea19386822b22f1b71353799b57
Oscar Andrade <ailezx@gmail.com>
2021-10-30 22:49:11 -0500

    Oscar Andrade - Correccion de script Model

---
+++ Assets/Scripts/SelectorModel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorModel : MonoBehaviour
+{
+    public GameObject Arquero;
+    public GameObject Mago;
+    public GameObject Caballero;
+    public GameObject Sacerdote;
+    public int counter;
+
+}
