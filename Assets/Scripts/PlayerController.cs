4f527172d4c8500af0cd49ad165d61ac9c1d7190
Oscar Andrade <ailezx@gmail.com>
2021-10-30 19:21:39 -0500

    Oscar Andrade - Finalizado script de PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public PlayerModel model;
+
+    private void Update()
+    {
+        PlayerMovement();
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ButtonSelect();
+        }
+    }
+    void PlayerMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            model.position += new Vector3(0, 2, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            model.position += new Vector3(0, -2, 0);
+        }
+    }
+    void ButtonSelect()
+    {
+        if (model.position == new Vector3(-8, 3.3f, 0))
+        {
+            SceneManager.LoadScene("CharacterSelect");
+        }
+        else
+        {
+            Debug.Log("Boton presionado, pero aqui no hay nada!");
+        }
+    }
 }
